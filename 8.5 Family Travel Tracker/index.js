32601971cebbacdf1a9f16e50ffcd0132a42adf5
razahaider03 <razahaider03@gmail.com>
2024-09-18 09:00:59 +0530

    Work index page

--- 8.5 Family Travel Tracker/index.js
+++ 8.5 Family Travel Tracker/index.js
@@ -19,6 +19,7 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 let users = [];
+
 async function fetchUsers() {
   // console.log((await db.query("SELECT * FROM users;")).rows);
   users = (await db.query("SELECT * FROM users;")).rows;
@@ -26,10 +27,10 @@ async function fetchUsers() {
 }
 
 
-
-async function checkVisisted() {
+async function checkVisisted(currentUserId) {
+  console.log(currentUserId);
   const result = await db.query(
-    "SELECT country_code FROM visited_countries JOIN users ON users.id = visited_countries.id WHERE users.id = $1;", [1]
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = visited_countries.user_id WHERE users.id = $1;", [currentUserId]
   );
   let countries = [];
   result.rows.forEach((country) => {
@@ -37,8 +38,10 @@ async function checkVisisted() {
   });
   return countries;
 }
+
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
+  const countries = await checkVisisted(currentUserId);
+  console.log(countries);
   const fetchedUsers = await fetchUsers();
   console.log(fetchedUsers);
   res.render("index.ejs", {
@@ -58,11 +61,12 @@ app.post("/add", async (req, res) => {
     );
 
     const data = result.rows[0];
+    console.log(data);
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2) ",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -73,7 +77,16 @@ app.post("/add", async (req, res) => {
   }
 });
 app.post("/user", async (req, res) => {
-  const users = await db.query("SELECT name FROM users;");
+  currentUserId = parseInt(req.body.id);
+  // console.log(currentUserId)
+  const countries = await checkVisisted(currentUserId);
+  console.log(countries)
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
   
 });
 
