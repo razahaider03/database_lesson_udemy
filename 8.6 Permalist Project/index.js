9a5e8c0ecffe20bc8eb269d81a913f9a53281777
razahaider03 <razahaider03@gmail.com>
2024-10-06 18:27:04 +0530

    Connect Dtatabase

--- 8.6 Permalist Project/index.js
+++ 8.6 Permalist Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -7,10 +8,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "776286",
+  port: 5432,
+});
+db.connect();
+
+let items = await getItems();
+
+async function getItems() {
+  const response = await db.query("SELECT * FROM public.items;")
+  const alItems = response.rows
+  return alItems;
+ 
+}
+
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {
